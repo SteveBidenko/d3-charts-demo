dd131a909b2af7e478aa7ccc099a12e0307517e5
Steve Bidenko <stanislav_bidenko@seven.ua>
2016-07-06 19:11:39 +0300

    Added dynamic tooltips to the chart

--- site/js/app.js
+++ site/js/app.js
@@ -19,22 +19,28 @@
         titleColors = ['red', 'orange', 'green', 'green', 'green'],
         y = d3.scale.linear().domain([0, upperRangeY]).range(yRange),
         x = d3.scale.linear().domain([0, upperRangeX]).range(xRange),
-        g, current;
+        g;
 
     $(document).ready(function() {
         normalizeDataY();
         subMetricChange();
         draw();
+        /*
+        setTimeout(function() {
+            $('svg circle').tipsy('show');
+        }, duration);
+        */
     });
     function normalizeDataY() {
+        var lastValue = params[lastNumber];
         dataY = params.map(function(el) {
             return el;
         });
-        dataY.splice(lastNumber + 1, 0, params[lastNumber], params[lastNumber]);
+        dataY.splice(lastNumber + 1, 0, lastValue, lastValue);
         console.log(dataY);
     }
     // Draw the chart at the start of application
-    function firstDraw() {
+    function prepareDraw() {
         vis = d3.select(idHtmlElement)
             .append('svg:svg')
             .attr('width', w)
@@ -44,21 +50,21 @@
             .attr('transform', 'translate(0, ' + (h - margin) + ')');
         // Lower line
         g.append('svg:line')
-            .attr('class', 'intermediate-lines')
+            .attr('class', 'lower-line')
             .attr('x1', x(0))
             .attr('y1', -1 * y(0))
-            .attr('x2', x(w))
+            .attr('x2', w)
             .attr('y2', -1 * y(0));
-        var xLabels = g.selectAll('.xLabel').data(x.ticks(upperRangeX));
+        var xLabels = g.selectAll('.xLabel').data(x.ticks(upperRangeX)).enter();
         // X labels
-        xLabels.enter().append('svg:text')
+        xLabels.append('svg:text')
             .attr('class', 'xLabel')
             .text(function(d, i) { return label1X[i - 1]; })
             .attr('x', function(d) { return x(d - 1) + margin; })
             .attr('y', 0)
             .attr('text-anchor', 'middle');
         // X labels (line 2)
-        xLabels.enter().append('svg:text')
+        xLabels.append('svg:text')
             .attr('class', 'xLabel')
             .style('font-weight', 'bold')
             .text(function(d, i) { return label2X[i - 1]; })
@@ -74,51 +80,43 @@
             .attr('x', 0)
             .attr('y', function(d) { return -1 * y(d - 1); })
             .attr('text-anchor', 'right');
-        // Intermediate lines
+        // Draw intermediate lines
         g.append('svg:g')
             .attr('class', 'grid')
             .attr('transform', 'translate(' + margin + ', ' + (margin - h) + ')')
-            .call(makeYAxis());
-    }
-    // Draw Intermediate lines
-    function makeYAxis() {
-        return d3.svg.axis()
-            .scale(y)
-            .orient('left')
-            .ticks(10)
-            .tickSize(-w, 0, 0)
-            .tickFormat('');
+            .call(function() {
+                return d3.svg.axis()
+                    .scale(y)
+                    .orient('left')
+                    .ticks(10)
+                    .tickSize(-w, 0, 0)
+                    .tickFormat('');
+            }());
     }
     // Draw a chart
-    function draw(id) {
+    function draw() {
+/*
         var line = d3.svg.line()
-                .x(function(d, i) {
-                    var res = x(i) + margin;
-                    // console.log(res);
-                    return res;
-                })
-                .y(function(d) {
-                    var res = -1 * y(d);
-                    // console.log(res);
-                    return res;
-                });
+                .x(function(d, i) { return x(i) + margin; })
+                .y(function(d) { return -1 * y(d); });
+*/
 
         vis = d3.select(idHtmlElement).select('svg').select('g');
 
         if (vis.empty()) {
-            firstDraw();
+            prepareDraw();
         }
         // Upper green line
         var upperLine = -1 * y(d3.max(dataY));
         g.append('svg:line')
-            .attr('class', 'upper-line')
+            .attr('class', 'upper-line dynamic')
             .attr('x1', x(0))
             .attr('y1', upperLine)
             .attr('x2', x(w))
             .attr('y2', upperLine);
-        // Draw two lines
+        // Draw primary line #1
         g.append('svg:line')
-            .attr('class', 'primary-line')
+            .attr('class', 'primary-line dynamic')
             .attr('x1', x(0) + margin)
             .attr('x2', x(1) + margin)
             .attr('y1', 0)
@@ -126,9 +124,9 @@
             .transition().duration(duration).ease(ease)
             .attr('y1', -1 * y(dataY[0]))
             .attr('y2', -1 * y(dataY[1]));
-
+        // Draw primary line #2
         g.append('svg:line')
-            .attr('class', 'line-to-future')
+            .attr('class', 'line-to-future dynamic')
             .attr('x1', x(1) + margin)
             .attr('x2', x(4) + margin)
             .attr('y1', 0)
@@ -136,46 +134,54 @@
             .transition().duration(duration).ease(ease)
             .attr('y1', -1 * y(dataY[1]))
             .attr('y2', -1 * y(dataY[lastNumber]));
-        // Draw three dots
-        g.append('svg:circle')
-            .attr('class', 'circle-0')
-            .style('stroke', titleColors[0])
-            .attr('title', dataY[0])
-            .attr('r', 6.5)
-            .attr('cx', x(0) + margin)
-            .attr('cy', 0)
-            .transition().duration(duration).ease(ease)
-            .attr('cy', -1 * y(dataY[0]));
-
-        g.append('svg:circle')
-            .attr('class', 'circle-1')
-            .style('stroke', titleColors[1])
-            .attr('title', dataY[1])
-            .attr('r', 6.5)
-            .attr('cx', x(1) + margin)
-            .attr('cy', 0)
-            .transition().duration(duration).ease(ease)
-            .attr('cy', -1 * y(dataY[1]));
-
-        g.append('svg:circle')
-            .attr('class', 'circle-1')
-            .style('stroke', titleColors[lastNumber])
-            .attr('title', dataY[lastNumber])
-            .attr('r', 6.5)
-            .attr('cx', x(4) + margin)
-            .attr('cy', 0)
-            .transition().duration(duration).ease(ease)
-            .attr('cy', -1 * y(dataY[lastNumber]));
-
-        current = id;
-        console.log('current = ' + current, 'data = ' + dataY);
+        // Draw lines, three dots and tooltips
+        [0, 1, 4].forEach(function(num) {
+            // Draw a vertical line
+            g.append('svg:line')
+                .attr('class', 'vertical-line dynamic')
+                .attr('x1', x(num) + margin)
+                .attr('y1', -margin)
+                .attr('x2', x(num) + margin)
+                .attr('y2', 0)
+                .transition().duration(duration).ease(ease)
+                .attr('y2', -1 * y(dataY[num]));
+            // Draw a dot
+            g.append('svg:circle')
+                .attr('class', 'dynamic')
+                .style('stroke', titleColors[num])
+                .attr('title', dataY[num])
+                .attr('r', 6.5)
+                .attr('cx', x(num) + margin)
+                .attr('cy', 0)
+                .transition().duration(duration).ease(ease)
+                .attr('cy', -1 * y(dataY[num]));
+            // Draw a tooltip
+/*
+            g.append('svg:text')
+                .attr('class', 'tipsy dynamic')
+                .attr('text-anchor', 'center')
+                .text(dataY[num])
+                .attr('x', x(num) + margin - 6.5)
+                .attr('y', 0)
+                .transition().duration(duration).ease(ease)
+                .attr('y', -1 * y(dataY[num]) - 6.5);
+*/
+        });
 
         $('svg circle').tipsy({
             gravity: 's',
-            html: true,
+            html: false,
             fade: true,
             // trigger: 'manual',
-            opacity: 0.85
+            opacity: 0.85,
+            onShow: function(tipsy, element) {
+                var borderColor = $(element).css('stroke');
+                $(tipsy).css({
+                    // 'color': borderColor,
+                    'border-color': borderColor
+                });
+                console.log(borderColor);
+            }
         });
     }
 
@@ -190,14 +196,12 @@
 
     function subMetricChange() {
         $('button[type="submit"]').on('click', function(e) {
-            d3.selectAll('circle')
+            d3.selectAll('circle.dynamic')
                 .transition().duration(duration).ease(ease)
                 .attr('cy', 0)
                 .attr('r', 0)
                 .remove();
-            d3.selectAll('line.upper-line').remove();
-            d3.selectAll('line.primary-line').remove();
-            d3.selectAll('line.line-to-future').remove();
+            d3.selectAll('line.dynamic, text.dynamic').remove();
 
             $(this).parent().addClass('active');
             generateOtherData();
